88174a8dbc081dbb07b1cc017faa23d19f491860
Hadi <53904259+hforoughmand@users.noreply.github.com>
2023-10-10 12:48:43 +0200

    Update run-beast-DTA2.sh

--- analyses/phylogenetic/run-beast-DTA2.sh
+++ analyses/phylogenetic/run-beast-DTA2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-__conda_setup="$('/home/hforoughmand/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-eval "$__conda_setup"
-#/home/hforoughmand/miniconda3/etc/profile.d/conda.sh
-export PATH="/home/hforoughmand/miniconda3/bin:$PATH"
+#__conda_setup="$('/home/hforoughmand/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+#eval "$__conda_setup"
+##/home/hforoughmand/miniconda3/etc/profile.d/conda.sh
+#export PATH="/home/hforoughmand/miniconda3/bin:$PATH"
 
 conda activate covid-uk
 
@@ -15,14 +15,15 @@ DIR=$1
 DATE_TREE=$2
 X=$3
 
-cd /net/viral_genomics/covid-lineage/huge-lineage-dynamics/analyses/phylogenetic/results/beast/
+#cd /net/viral_genomics/covid-lineage/huge-lineage-dynamics/analyses/phylogenetic/results/beast/
+cd results/beast/
 #rm -rf $DIR
 #mkdir $DIR/
 cp $X-DTA2-$DATE_TREE.xml $DIR/
 echo "executing beast"
 cd $DIR/
 
-export PATH=/net/viral_genomics/covid-lineage/germany-lineage-dynamics/bin/beast/bin:$PATH
+#export PATH=/net/viral_genomics/covid-lineage/germany-lineage-dynamics/bin/beast/bin:$PATH
 beast-c8cc55d4fe -save_every 1000000 -overwrite -save_state $X-DTA2-$DATE_TREE.state $X-DTA2-$DATE_TREE.xml
 
 echo "done"

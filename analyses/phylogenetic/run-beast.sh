b9ec2fe6550d3347a538b3e5e7e711b193a2661a
Hadi <53904259+hforoughmand@users.noreply.github.com>
2023-10-10 16:15:15 +0200

    Update run-beast.sh

--- analyses/phylogenetic/run-beast.sh
+++ analyses/phylogenetic/run-beast.sh
@@ -24,6 +24,6 @@ echo "executing beast"
 cd $DIR/
 
 #export PATH=/net/viral_genomics/covid-lineage/germany-lineage-dynamics/bin/beast/bin:$PATH
-beast-v1.10.5pre_thorney_0.1.1 -save_every 1000000 -save_state $X.fixedRootPrior.skygrid-$DATE_TREE.state $X.fixedRootPrior.skygrid-$DATE_TREE.xml 
+beast -save_every 1000000 -save_state $X.fixedRootPrior.skygrid-$DATE_TREE.state $X.fixedRootPrior.skygrid-$DATE_TREE.xml 
 
 echo "done"
